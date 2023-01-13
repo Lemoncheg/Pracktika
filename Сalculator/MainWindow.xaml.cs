5091f5bed99540f0535cf3a5017858843ef72002
Lemoncheg <Lojer8899@gmail.com>
2023-01-13 13:50:20 +0300

    11

--- Сalculator/MainWindow.xaml.cs
+++ Сalculator/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace Сalculator
     {
         private double value1 = 0;
         private double value2 = 0;
+        private double result;
+        private double degres;
         public MainWindow()
         {
             InitializeComponent();
@@ -33,88 +35,109 @@ namespace Сalculator
             {
                 value1 = Value_1.Text != "" ? Convert.ToDouble(Value_1.Text): 0;
                 value2 = Convert.ToDouble(Value_2.Text);
+
+                if (ComboDegReg.SelectedIndex == 0)
+                {
+                    degres = Math.PI / 180;
+                }
+                else
+                    degres = 1;
+
                 switch (Combo_Function.SelectedIndex)
                 {
                     case 0:
-                        ResultLabel.Content = "" + Math.Round((value1 + value2),4);
+                        Math.Round(value1 + value2, 4);
                         break;
                     case 1:
-                        ResultLabel.Content = "" + Math.Round((value1 - value2),4);
+                        result = Math.Round(value1 - value2,4);
                         break;
                     case 2:
                         if(value1 == 0)
                         {
                             MessageBox.Show("Деление на ноль", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                         }
-                        ResultLabel.Content = "" + Math.Round((value1 / value2),4);
+                        result = Math.Round(value1 / value2, 4);
                         break;
                     case 3:
-                        ResultLabel.Content = "" + Math.Round((value1 * value2), 4);
+                        result = Math.Round(value1 * value2, 4);
                         break;
                     case 4:
-                        ResultLabel.Content = "" + Math.Round(Math.Sin(value2), 1);
+<<<<<<< HEAD
+                        result = Math.Round(Math.Sin(value2 * degres) , 4);
+=======
+                        ResultLabel.Content = "" + Math.Round(Math.Sin(value2), 4);
+>>>>>>> d84f6903a79d0db762dd57471cc6066d84d02c8f
                         break;
                     case 5:
-                        ResultLabel.Content = "" + Math.Round((Math.Cos(value2)), 4);
+                        result = Math.Round(Math.Cos(value2 * degres), 4);
                         break;
                     case 6:
-                        ResultLabel.Content = "" + Math.Round((Math.Sin(value2) / Math.Cos(value2)), 4);
+                        result = Math.Round(Math.Sin(value2 * degres) / Math.Cos(value2 * degres), 4);
                         break;
                     case 7:
-                        ResultLabel.Content = "" + Math.Round((Math.Cos(value2) / Math.Sin(value2)), 4);
+                        result = Math.Round(Math.Cos(value2 * degres) / Math.Sin(value2 * degres), 4);
                         break;
                 }
+                ResultLabel.Content = "" + result;
             }
-            catch(Exception ex)
+            catch
             {
-                MessageBox.Show("" + ex, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Неккоректный ввод данных", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
         private void PlusButton_Click(object sender, RoutedEventArgs e)
         {
             Value_1.Visibility = Visibility.Visible;
             ValueText.Visibility = Visibility.Visible;
+            ComboDegReg.Visibility = Visibility.Hidden;
         }
 
         private void MinusButton_Click(object sender, RoutedEventArgs e)
         {
             Value_1.Visibility = Visibility.Visible;
             ValueText.Visibility = Visibility.Visible;
+            ComboDegReg.Visibility = Visibility.Hidden;
         }
 
         private void DelenieButton_Click(object sender, RoutedEventArgs e)
         {
             Value_1.Visibility = Visibility.Visible;
             ValueText.Visibility = Visibility.Visible;
+            ComboDegReg.Visibility = Visibility.Hidden;
         }
 
         private void UmnojenieButton_Click(object sender, RoutedEventArgs e)
         {
             Value_1.Visibility = Visibility.Visible;
             ValueText.Visibility = Visibility.Visible;
+            ComboDegReg.Visibility = Visibility.Hidden;
         }
         private void SinButton_Click(object sender, RoutedEventArgs e)
         {
             Value_1.Visibility = Visibility.Hidden;
             ValueText.Visibility = Visibility.Hidden;
+            ComboDegReg.Visibility = Visibility.Visible;
         }
 
         private void CosButton_Click(object sender, RoutedEventArgs e)
         {
             Value_1.Visibility = Visibility.Hidden;
             ValueText.Visibility = Visibility.Hidden;
+            ComboDegReg.Visibility = Visibility.Visible;
         }
 
         private void TngButton_Click(object sender, RoutedEventArgs e)
         {
             Value_1.Visibility = Visibility.Hidden;
             ValueText.Visibility = Visibility.Hidden;
+            ComboDegReg.Visibility = Visibility.Visible;
         }
 
         private void CtngButton_Click(object sender, RoutedEventArgs e)
         {
             Value_1.Visibility = Visibility.Hidden;
             ValueText.Visibility = Visibility.Hidden;
+            ComboDegReg.Visibility = Visibility.Visible;
         }
     }
 }
