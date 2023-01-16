e690af5c64fcc45c0bf9860c29d1b1f8324712ee
Lemoncheg <Lojer8899@gmail.com>
2023-01-16 13:15:38 +0300

    20.5

--- Task 2/Window3.xaml.cs
+++ Task 2/Window3.xaml.cs
@@ -30,28 +30,40 @@ namespace Task_2
         {
             try
             {
-                i++;
+                if(BoxAnswer.Text == "")
+                {
+                    QuestionsLabel.Content = "Ответь на вопрос";
+                    QuestionsLabel.Foreground = Brushes.Red;
+                    i--;
+                }
+                else
+                    i++;
                 switch (i)
                 {
                     case 1:
+                        QuestionsLabel.Foreground = Brushes.GreenYellow;
                         result += $"Ваше имя: {BoxAnswer.Text}";
                         QuestionsLabel.Content = "Сколько вам лет?";
                         break;
                     case 2:
+                        QuestionsLabel.Foreground = Brushes.GreenYellow;
                         result += $"\nВаш возрост: {Convert.ToInt32(BoxAnswer.Text)}";
                         QuestionsLabel.Content = "Ваш знак зодиака?";
                         break;
                     case 3:
+                        QuestionsLabel.Foreground = Brushes.GreenYellow;
                         result += $"\nПо знаку задиака вы: {BoxAnswer.Text}";
-                        QuestionsLabel.Content = "Кем вы работатете";
+                        QuestionsLabel.Content = "Кем вы работатете?";
                         break;
                     case 4:
+                        QuestionsLabel.Foreground = Brushes.GreenYellow;
                         result += $"\nВы роботаете: {(BoxAnswer.Text)}";
                         MessageBox.Show(result, "Info", MessageBoxButton.OK, MessageBoxImage.Exclamation);
                         i = 0;
                         result = "";
                         break;
                     default:
+                        QuestionsLabel.Foreground = Brushes.GreenYellow;
                         QuestionsLabel.Content = "Как вас зовут?";
                         break;
                 }
@@ -59,6 +71,7 @@ namespace Task_2
 
             catch
             {
+                QuestionsLabel.Foreground = Brushes.Red;
                 QuestionsLabel.Content = "Ответь на вопрос";
                 if(i == 2)
                 {
